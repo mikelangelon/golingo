bf7a945c8491727f961977bbd334b628a7144a81
Miguel Corvo Diaz <mikelangelon@gmail.com>
2021-09-11 16:06:55 +0200

    Adding basic CLI with Cobra

---
+++ data/data.go
@@ -0,0 +1,37 @@
+package data
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+var db *sql.DB
+
+func OpenDatabase() error {
+	var err error
+
+	db, err = sql.Open("sqlite3", "./sqlite-database.db")
+	if err != nil {
+		return err
+	}
+
+	return db.Ping()
+}
+
+func CreateTable() {
+	createTableSQL := `CREATE TABLE IF NOT EXISTS words (
+		"idNote" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"word" TEXT,
+		"definition" TEXT,
+		"category" TEXT
+	  );`
+
+	statement, err := db.Prepare(createTableSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	statement.Exec()
+	log.Println("Golingo table created")
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/mikelangelon/golingo
+
+go 1.16
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.8
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/viper v1.8.1
+)
